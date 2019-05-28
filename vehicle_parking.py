fec29c284ea828c44edd36876af18427554ab172
vaishnavinambiar <45651562+vaishnavinambiar@users.noreply.github.com>
2019-05-28 11:39:50 +0530

    vehicle_parking.py

--- vehicle_parking.py
+++ vehicle_parking.py
@@ -1,19 +1,29 @@
+import logging
+logging.basicConfig(filename='parking.log',level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
+
 types = ['Car', 'Bike', 'Van', 'Bus']
 vehicles = []
+
 class Vehicle:
     def __init__(self,types):
         self.types=types
 
     def getTypes(self):
+        """To get which type of vehicle is coming to park"""
         if self.types in types:
             return "Parking allowed"
         else:
             return "no parking"
 
     def addvehicle(self):
-        if self.types in types:
-            vehicles.append(self.types)
-            return vehicles
+        """To park the vehicle"""
+        try:
+            if self.types in types:
+                vehicles.append(self.types)
+                logging.info("Type of vehicle parked is: %s ",vehicles)
+                return vehicles
+        except:
+            return "None"
 
             
 class ParkingLevel:
@@ -23,12 +33,15 @@ class ParkingLevel:
         self.limit = limit
 
     def getLevel(self):
+        """To get the level to park the vehicle"""
         if self.types in types:
+            logging.info("Available level is: %s",self.level_number)
             return 'park '+ self.types + ' in level ' + self.level_number
         else:
            return "not allowed"
 
     def createlevel(self):
+        """To create new level"""
         types_limit = { 'Car': 20, 'Bike': 50, 'Van':10, 'Bus':7}
         if self.types in types_limit.keys() and self.limit> types_limit[self.types]:
             return "go to next level and bus restricted"
@@ -41,11 +54,13 @@ class Exitpanel:
         self.types = types
 
     def removevehicle(self):
+        """To unpark the vehicle"""
         user_option = raw_input("Do you want to unpark? y/n ")
-        if user_option == 'y':
-            del vehicles[:]
-            return vehicles
-        else:
+        try:
+            if user_option == 'y':
+                del vehicles[:]
+                return vehicles
+        except ValueError:
             return "OK"
 
 vehicle = Vehicle('Car')
@@ -56,7 +71,6 @@ print level.getLevel()
 print level.createlevel()
 exitpanel = Exitpanel('Car')
 print exitpanel.removevehicle()
-
-
+       
                 
 
